1ed0725c265fc796b3a2e2c3583c70e38f99512c
Melvin <msaballos219@gmail.com>
2024-11-11 11:54:52 -0600

    Define student class with private member variables for data.

--- StudentRosterProject/student.h
+++ StudentRosterProject/student.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include <string>
+#include <array>
+#include "degree.h"
+
+class Student {
+private:
+	std::string studentID;
+	std::string firstName;
+	std::string lastName;
+	std::string emailAddress;
+	int age;
+	std::array<int, 3> daysToFinish;
+	DegreeProgram degreeProgram;
+};
