2d4d4235e9dd28b666b55a90c7eea6d0e4efd550
Melvin <msaballos219@gmail.com>
2024-11-11 15:26:44 -0600

    Changed variable value.

--- StudentRosterProject/StudentRosterProject.cpp
+++ StudentRosterProject/StudentRosterProject.cpp
@@ -20,7 +20,7 @@ int main() {
         "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
         "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
         "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-        "011768283,Melvin,Saballos,msaball@wgu.edu,29,28,28,28,SOFTWARE"
+        "A5,Melvin,Saballos,msaball@wgu.edu,29,28,28,28,SOFTWARE"
     };
 
     for (const auto& data : studentData) {
